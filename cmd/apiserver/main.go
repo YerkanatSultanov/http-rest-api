3132a2b6473a65b14c5ce1306f85a40d2f11323b
ysultanov <erkanatsultanov1999@gmail.com>
2023-09-22 17:12:29 +0600

    initial

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"flag"
+	"github.com/BurntSushi/toml"
 	"github.com/YerkanatSultanov/http-rest-api/internal/app/apiserver"
 	"log"
 )
 
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/apiserver.toml", "path to config file")
+
+}
+
 func main() {
-	api := apiserver.New()
+	flag.Parse()
+
+	config := apiserver.NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	api := apiserver.New(config)
 
 	if err := api.Start(); err != nil {
 		log.Fatal(err)
